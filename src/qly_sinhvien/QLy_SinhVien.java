6dab40dc30c1a525d121bd4d45ba54516595f196
toanf2106 <phamtoan17tb@gmail.com>
2025-04-02 17:17:40 +0700

    first commit

---
+++ src/qly_sinhvien/QLy_SinhVien.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package qly_sinhvien;
+
+import View.TrangChu;
+
+/**
+ *
+ * @author phamt
+ */
+public class QLy_SinhVien {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new TrangChu().setVisible(true);
+    }
+    
+}
